767c92624bd4223c0ce073948efafd57af6e92b0
hanifabo <hanifabo@gmail.com>
2017-03-14 17:24:49 -0700

    test ride

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,3 +5,15 @@ var APP = (function (app) {
 
   return app;
 }(APP || {}));
+
+var eventManager = new EventManager();
+
+
+
+    $(document).ready(function(){
+
+        $("#openModal").on("click",function () {
+            eventManager.evenApiCaller();
+        })
+
+    })
