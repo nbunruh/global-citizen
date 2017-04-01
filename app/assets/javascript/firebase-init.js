12a967e901910b135f7f48e7252a21c418cc5b10
HYUNGWU PAE <monad98@naver.com>
2017-04-01 09:01:11 -0700

    api-key activated

--- app/assets/javascript/firebase-init.js
+++ app/assets/javascript/firebase-init.js
@@ -1,9 +1,10 @@
 var APP = (function (app) {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyAumyYYmzpHea4zyErWSwq613w4nXp8rJ8",
+    apiKey: "AIzaSyAxRSX5Q7qHZi-tBSUhOtl_zn5S757QLUw",
     authDomain: "global-citizen.firebaseapp.com",
     databaseURL: "https://global-citizen.firebaseio.com",
+    projectId: "global-citizen",
     storageBucket: "global-citizen.appspot.com",
     messagingSenderId: "367570053327"
   };
